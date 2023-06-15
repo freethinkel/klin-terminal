d558e56f1dcd89ee627069b8a29edfbebbfc5acd
Egor Kolesnikov <freethinkel@gmail.com>
2023-06-15 19:16:33 +0600

    fix(build): fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 flutter build macos --release
+rm -rf ./build/macos/Cheber\ Terminal.app
 mv ./build/macos/Build/Products/Release/cheber_terminal.app ./build/macos/Cheber\ Terminal.app
 rm -rf ./CheberTerminal.dmg
 npx appdmg ./create_dmg.json ./CheberTerminal.dmg
